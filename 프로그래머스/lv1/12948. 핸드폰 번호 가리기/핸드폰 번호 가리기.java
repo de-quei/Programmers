8982e2718636eb2c6d35484a10036430f4ee29dd
Hyunji Kim <s2208@e-mirim.hs.kr>
2023-08-28 11:54:29 +0900

    Update 핸드폰 번호 가리기.java

--- 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -1,12 +1,11 @@
 class Solution {
     public String solution(String phone_number) {
         String answer = "";
-        char[] arr = phone_number.toCharArray();
         
-        //만약 01029523435라면 문자길이 -4
-        for(int i = 0; i < arr.length; i++){
-            if(i < arr.length - 4) answer += "*";
-            else answer += arr[i];
+        //문자길이-4 부터 숫자가 들어감.
+        for(int i = 0; i < phone_number.length(); i++){
+            if(i < phone_number.length() - 4) answer += "*";
+            else answer += phone_number.charAt(i);
         }
         return answer;
     }
