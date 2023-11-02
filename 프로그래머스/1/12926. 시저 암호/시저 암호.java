ca30e78c5b28e5f52ec8597e5cfa76f0cf78b2cc
김현지 <s2208@e-mirim.hs.kr>
2023-11-02 10:20:30 +0900

    주석 추가

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -1,27 +1,32 @@
 class Solution {
     public String solution(String s, int n) {
-        //A == 65 / a == 97
+        //A == 65 / a == 97 (아스키코드)
         String answer = "";
         
         for(int i = 0; i < s.length(); i++){
             char ch = s.charAt(i);
+            //공백이라면 똑같이 공백 추가
             if(ch == ' '){
                 answer += " ";
             }
             //대문자라면
             else if(ch >= 65 && ch <= 90){
-                if(ch + n > 'Z'){
-                    answer += (char)(ch-26+n);
+                //만약 Z를 넘으면
+                if(ch + n > 90){
+                    //다시 앞부터 + n
+                    answer += (char)(ch - 26 + n);
                 }else{
-                    answer += (char)(ch+n);
+                    answer += (char)(ch + n);
                 }
             }
             //소문자라면
             else if(ch >= 97 && ch <= 122){
-                if(ch + n > 'z'){
-                    answer += (char)(ch-26+n);
+                //만약 z를 넘으면
+                if(ch + n > 122){
+                    //다시 앞부터 + n
+                    answer += (char)(ch - 26 + n);
                 }else{
-                    answer += (char)(ch+n);
+                    answer += (char)(ch + n);
                 }
             }
         }
