8767fd2d62e41d76abc5efe26dd18f29a2b40a29
김현지 <s2208@e-mirim.hs.kr>
2023-11-02 10:05:33 +0900

    삼항연산자 방식 추가

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -2,12 +2,17 @@ class Solution {
     public String solution(int n) {
         String answer = "";
         
+        // for(int i = 1; i <= n; i++){
+        //     if(i%2 != 0){
+        //         answer += "수";
+        //     }else {
+        //         answer += "박";
+        //     }
+        // }
+
+        //단순 if~else는 3항 연산자 사용해도 좋을듯!
         for(int i = 1; i <= n; i++){
-            if(i%2 != 0){
-                answer += "수";
-            }else {
-                answer += "박";
-            }
+            answer += (i%2 != 0) ? "수" : "박";
         }
         return answer;
     }
