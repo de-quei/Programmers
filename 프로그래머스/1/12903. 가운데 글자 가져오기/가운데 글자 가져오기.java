88e6e4216f36285aafd9a0bec4315819dde276d6
김현지 <s2208@e-mirim.hs.kr>
2023-12-16 21:17:39 +0900

    Update 가운데 글자 가져오기.java

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -1,11 +1,11 @@
 class Solution {
     public String solution(String s) {
         String answer = "";
-        int mid = s.length() / 2;
+        int mid = s.length() / 2; //가운데 위치 지정
         
         for(int i = 0; i < s.length(); i++){
-            if(s.length() % 2 == 0) answer = s.substring(mid-1, mid+1);
-            else answer = s.substring(mid, mid + 1);
+            if(s.length() % 2 == 0) answer = s.substring(mid-1, mid+1); //문자열 길이가 짝수라면 가운데 두 글자 추출
+            else answer = s.substring(mid, mid + 1); //문자열 길이가 홀수라면 가운데 한 글자 추출
         }
         return answer;
     }
