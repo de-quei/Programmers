0dab4dd2fb9720cd0014ac7f7989169832786807
김현지 <s2208@e-mirim.hs.kr>
2024-10-15 15:42:44 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -0,0 +1,14 @@
+function solution(my_string, num1, num2) {
+    var answer = '';
+    
+    var arr = my_string.split("");
+    var temp = '';
+  
+    temp = arr[num1];
+    arr[num1] = arr[num2];
+    arr[num2] = temp;
+    
+    answer = arr.join('');
+    
+    return answer;
+}
