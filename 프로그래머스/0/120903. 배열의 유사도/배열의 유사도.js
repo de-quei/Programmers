8e3369902f0372b8d8d8cdeeac75afa1b3364b12
김현지 <s2208@e-mirim.hs.kr>
2024-10-16 13:44:10 +0900

    Update 배열의 유사도.js

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
@@ -1,10 +1,13 @@
 function solution(s1, s2) {
-    var answer = 0;
+    // var answer = 0;
     
-    const new_arr = s1.concat(s2);
-    const set = new Set(new_arr);
+    // const new_arr = s1.concat(s2);
+    // const set = new Set(new_arr);
     
-    answer = new_arr.length - set.size;
+    // answer = new_arr.length - set.size;
     
-    return answer;
+    // return answer;
+
+    // filter 함수 사용
+    return s1.filter((e) => s2.includes(e)).length;
 }
